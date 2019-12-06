637b5b4cde98633ce1bd9787f177b8c0f05bb577
NamikoToriyama <g1720529@is.ocha.ac.jp>
2019-12-06 21:02:50 +0900

    138B問題

--- 130/8/b.cpp
+++ 130/8/b.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iomanip>
+
 //#include <tuple>
 
 //#include <bits/stdc++.h>
@@ -13,12 +15,18 @@ using namespace std;
 
 int main() {
   int N, a;
+  double p = 0;
+  double result;
   cin >> N;
 
+  // これで通るのかウケる
   for (int i = 0; i < N; i++) {
     cin >> a;
+    p += 1./a;
   }
+
+  result = 1/p;
   
-  cout << N << endl;
+  cout << setprecision(10) << result << endl;
 
 }
