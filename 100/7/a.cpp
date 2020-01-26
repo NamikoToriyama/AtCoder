9dd4ef5198432ed0a28c8aae5b1a280110ac4592
NamikoToriyama <g1720529@is.ocha.ac.jp>
2020-01-27 02:57:19 +0900

    add 107

---
+++ 100/7/a.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+//#include <tuple>
+
+//#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (int)n; i++)
+#define ll long long
+#define pb push_back
+
+int main() {
+  int N, i;
+  cin >> N >> i;
+  
+  cout <<  N + 1 - i << endl;
+
+}
