58bcd1b25c3281fddc524c07b60a78843bd63cce
NamikoToriyama <g1720529@is.ocha.ac.jp>
2020-02-26 02:29:49 +0900

    add file

--- 130/6/d.cpp
+++ 130/6/d.cpp
@@ -14,12 +14,36 @@ using namespace std;
 #define ll long long
 #define pb push_back
 
+void print_lr(vector<int> lr){
+  for(int i: lr){
+    cout << i << " ";
+  }
+  cout << endl;
+}
+
+vector<int> gather(string s, vector<int> lr){
+  int N = s.size();
+  vector<int> x(N, 0);
+  rep(i, N){
+    if(s[i] ==  'R'){
+      x[i+1] += lr[i];
+    } else {// それ以外
+      x[i-1] += lr[i];
+    }
+  }
+  return x;
+}
+
 int main() {
-  int N;
   string s;
   cin >> s;
-  N = s.size();
   
-  cout << setprecision(7) << N << endl;
-
+  int N = s.size();
+  vector<int> lr(N, 1);
+  // 2パターン
+  rep(i, 100000){
+    lr = gather(s, lr);
+  }
+  
+  print_lr(lr);
 }
