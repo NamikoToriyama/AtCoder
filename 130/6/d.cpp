021cca426e481fc27fcfa8ab9a953d51fec9a4d8
NamikoToriyama <g1720529@is.ocha.ac.jp>
2020-02-27 16:14:59 +0900

    add code

--- 130/6/d.cpp
+++ 130/6/d.cpp
@@ -5,14 +5,15 @@
 #include <vector>
 #include <algorithm>
 #include <iomanip>
-
-//#include <tuple>
-
+/// This is KUSO Code!!!!!
 //#include <bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for(int i = 0; i < (int)n; i++)
 #define ll long long
 #define pb push_back
+
+// スマートすぎる回答
+// https://atcoder.jp/contests/abc136/submissions/10368936
 
 void print_lr(vector<int> lr){
   for(int i: lr){
@@ -21,14 +22,55 @@ void print_lr(vector<int> lr){
   cout << endl;
 }
 
-vector<int> gather(string s, vector<int> lr){
+vector<pair<char, int> > RunLengthEncoding(string s){
+  char c = s[0];
+  int cnt = 0;
+  vector<pair<char, int> > rle;
+  for(char i: s){
+    if(c==i){
+      cnt++;
+    } else {
+      rle.push_back(make_pair(c, cnt));
+      c = i;
+      cnt = 1;
+    }
+  }
+  rle.push_back(make_pair(c, cnt));
+  // デバッグ用
+  // for(pair<char, int> p : rle){
+  //   cout << p.first << " " << p.second << endl;
+  // }
+  return rle;
+}
+
+vector<int> gather(string s){
   int N = s.size();
   vector<int> x(N, 0);
+  vector<pair<char, int> > rle = RunLengthEncoding(s);
+  int num = rle[0].second - 1, j = 0;
+  int r_num, l_num, ave, tmp, sum;
   rep(i, N){
-    if(s[i] ==  'R'){
-      x[i+1] += lr[i];
-    } else {// それ以外
-      x[i-1] += lr[i];
+    if(i == num){
+      r_num = rle[j].second;
+      l_num = rle[j+1].second;
+      ave = (r_num + l_num)/2;
+      sum = r_num+l_num;
+      if(sum%2==0) { // 合計が2で割れる場合
+        r_num = ave;
+        l_num = ave;
+      } else { // 合計が2で割れない場合
+          r_num = ave + abs(sum - r_num+1)%2;
+          l_num = ave + abs(sum - l_num+1)%2;
+      }
+      x[i] = r_num;
+      x[i+1] = l_num;
+      i++; // 0を代入しないで欲しいので二つ進める
+      if (j + 2 < rle.size()){
+        num += rle[j+1].second + rle[j+2].second;
+        j += 2;
+      }
+    } else {
+      x[i] = 0;
     }
   }
   return x;
@@ -37,13 +79,9 @@ vector<int> gather(string s, vector<int> lr){
 int main() {
   string s;
   cin >> s;
-  
   int N = s.size();
   vector<int> lr(N, 1);
-  // 2パターン
-  rep(i, 100000){
-    lr = gather(s, lr);
-  }
   
+  lr = gather(s);
   print_lr(lr);
 }
