c3a4d4b53d7ada2515959342f6f45da772b6ae93
NamikoToriyama <g1720529@is.ocha.ac.jp>
2020-05-13 03:07:01 +0900

    add files

---
+++ 70/7/a.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <queue>
+#include <map>
+
+//#include <bits/stdc++.h>
+using namespace std;
+#define print(x) cout<<(x)<<endl
+#define rep(i, n) for(int i = 0; i < (int)n; i++)
+#define ll long long int
+#define pb push_back
+
+int main() {
+  string s, t;
+  cin >> s >> t;
+  if(s[0]==t[2] && s[1]==t[1] && s[2]==t[0]) print("YES");
+  else print("NO");
+
+}
