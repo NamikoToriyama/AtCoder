c4be7b91bddd56186b95d2ea66bee09f1e62875b
NamikoToriyama <g1720529@is.ocha.ac.jp>
2020-06-05 00:10:50 +0900

    add files

---
+++ 30/1/b.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <queue>
+#include <map>
+
+//#include <bits/stdc++.h>
+using namespace std;
+#define print(x) cout<<(x)<<endl
+#define prints(x, y) cout<<(x)<<" "<<(y)<<endl
+#define rep(i, n) for(int i = 0; i < (int)n; i++)
+#define r_rep(i, n) for(int i = (int)n-1; i >= 0; i--)
+#define ll long long int
+#define pb push_back
+const long long MOD = 1000000007;
+const long long INF = 1LL << 60;
+
+int main() {
+  int L, H, N, a;
+  cin >> L >> H >> N;
+
+  rep(i, N){
+    cin >> a;
+    if(a < L) print(L-a);
+    else if (H < a) print(-1);
+    else if (a <= H) print(0); 
+  }
+
+}
